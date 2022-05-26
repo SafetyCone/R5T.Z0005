d90b31c24d4af2ffcbb7f61e0025fcbc6f8c1f4c
davidcoats <david@getsafetycone.com>
2022-05-26 11:45:06 -0700

    Initial commit of source-stub.

---
+++ source/R5T.Z0005/Code/Classes/Class1.cs
@@ -0,0 +1,9 @@
+using System;
+
+
+namespace R5T.Z0005
+{
+    public class Class1
+    {
+    }
+}
---
+++ source/R5T.Z0005/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.Z0005
+{
+    /// <summary>
+    /// Git-related values.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
